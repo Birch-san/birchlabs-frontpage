b3478cb6c0c46b3c47db8519993bc8fb4887e733
Alex Birch <vengeance.mx@gmail.com>
2016-03-05 22:52:31 +0000

    fine I'll use React _just_ so I can hot reloading of page portions (i.e. as jsx modules).

---
+++ serve.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# cd to dir of this script
+MYDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+pushd $MYDIR > /dev/null
+# upon any kind of termination, return to our original directory
+trap "popd > /dev/null" EXIT SIGHUP SIGINT SIGTERM
+
+#######
+
+# https://webpack.github.io/docs/hot-module-replacement-with-webpack.html
+
+webpack-dev-server ./index.jsx --hot --inline --module-bind "css=style\!css"
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './index.jsx',
+  output: {
+    filename: 'browser-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
