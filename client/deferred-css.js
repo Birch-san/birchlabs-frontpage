cf28ed6d61b507669dfa787469d7371a7f4ea884
Alex Birch <vengeance.mx@gmail.com>
2016-03-06 00:33:20 +0000

    Fix CSS when built

--- client/deferred-css.js
+++ client/deferred-css.js
@@ -1,7 +1,7 @@
 // Google PageSpeed non-blocking, post-initial-paint css loader https://developers.google.com/speed/docs/insights/OptimizeCSSDelivery#example
 // I shortened it for fun, and also added support for an array of stylesheets
 export default (requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame || window.addEventListener.bind(null, 'load'))(function() {
-  ["//cdnjs.cloudflare.com/ajax/libs/marx/1.3.0/marx.min.css"].forEach(function(s) {
+  ["https://cdnjs.cloudflare.com/ajax/libs/marx/1.3.0/marx.min.css"].forEach(function(s) {
     var l = document.createElement('link'); l.rel = 'stylesheet'; l.href = s;
     var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
   });
